95f47c9e6b8977592a5696b77029c788feaae5ba
Florian Angeletti <florian.angeletti@inria.fr>
2022-08-16 16:58:22 +0200

    Add Random3 and Random to signature consistency test

--- tests/consistency.ml
+++ tests/consistency.ml
@@ -1,9 +1,12 @@
-(* Check that only split is missing from implementation anterior to Random5 *)
+(* Check that only split is missing from implementations anterior to Random5 *)
 
+module type r3 = module type of Random3
 module type r4 = module type of Random4
 module type r5 = module type of Random5
 
-module F(R4:r4)(R5:r5): r5 = struct
+module R5_to_R4(R5:r5): r4 = R5
+
+module R4_to_R5(R4:r4): r5 = struct
   module State = struct
      include R4.State
      let split x = x
@@ -11,3 +14,13 @@ module F(R4:r4)(R5:r5): r5 = struct
   include (R4: r4 with module State := State)
   let split () = State.split (get_state ())
 end
+
+module R3_equal_R4(R3t:sig module type t = r3 end) :
+  sig module type t = r4 end = R3t
+
+#if OCAML_VERSION > (5,0,0)
+module R5_equal_Stdlib(R5t: sig module type t = r5 end):
+sig module type t = module type of Random end = struct
+   R5t
+  end
+#endif
