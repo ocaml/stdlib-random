91858d00b88c8111ad83382d717d78a7ad5947da
Florian Angeletti <florian.angeletti@inria.fr>
2022-08-17 13:07:32 +0200

    More signature consistency tests

--- tests/consistency.ml
+++ tests/consistency.ml
@@ -3,6 +3,7 @@
 module type r3 = module type of Random3
 module type r4 = module type of Random4
 module type r5 = module type of Random5
+module type r5o = module type of Random5o
 
 module R5_to_R4(R5:r5): r4 = R5
 
@@ -15,12 +16,17 @@ module R4_to_R5(R4:r4): r5 = struct
   let split () = State.split (get_state ())
 end
 
-module R3_equal_R4(R3t:sig module type t = r3 end) :
-  sig module type t = r4 end = R3t
+
+module R5o_equal_R5:sig module type t = r5 end = struct
+  module type t = r5o
+end
+
+module R3_equal_R4:sig module type t = r3 end = struct
+  module type t = r4
+end
 
 #if OCAML_VERSION > (5,0,0)
-module R5_equal_Stdlib(R5t: sig module type t = r5 end):
-sig module type t = module type of Random end = struct
-   R5t
-  end
+module R5_equal_Stdlib: sig module type t = r5 end = struct
+  module type t = module type of Random
+end
 #endif
